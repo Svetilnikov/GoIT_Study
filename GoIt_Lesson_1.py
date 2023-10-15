ae46400f0a762b946c8f5cc7ff15b7395068f164
“Andrey <srazuvsyo@gmail.com>
2023-10-15 04:01:55 +0300

    Good Luck

--- GoIt_Lesson_1.py
+++ GoIt_Lesson_1.py
@@ -1,2 +1,17 @@
 print("Hello World")
 print("Goodbye World")
+import math
+print(8 % 3)
+a = print(1.5 * pow(10, 4))
+a = abs(-15)
+print(a)
+a = round(6.586, 2)
+print(a)
+a = 1.5e4
+print(a)
+a = 45
+print(f"Variable is equal to: {a + 10}")
+a = "Tony"
+b = "Stark"
+result = a + " " + b
+print(result)
